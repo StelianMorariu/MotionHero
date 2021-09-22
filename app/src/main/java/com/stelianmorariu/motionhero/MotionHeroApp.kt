60b0d6e69d6c69b187fd6bcc1c86556f354a014d
Stelian Morariu <stelian.morariu@starlingbank.com>
2021-09-22 10:02:43 +0100

    boilerplate

---
+++ app/src/main/java/com/stelianmorariu/motionhero/MotionHeroApp.kt
@@ -0,0 +1,10 @@
+package com.stelianmorariu.motionhero
+
+import android.app.Application
+
+class MotionHeroApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,133 @@
+/*
+ * Copyright ©  2019 Stelian Morariu. All rights reserved.
+ */
+
+
+object Versions {
+    const val gradle = "3.4.0"
+
+    const val compileSdk = 28
+    const val minSdk = 23
+    const val targetSdk = 28
+
+
+    const val appcompat = "1.3.1"
+    const val annotation = "1.1.0"
+    const val material = "1.5.0-alpha03"
+    const val constraintLayout = "2.0.4"
+    const val supportFragment = "1.1.0-alpha09"
+    const val navigation = "2.1.0-alpha04"
+
+    const val preferences = "1.1.0-rc01"
+
+    const val dagger = "2.22.1"
+
+
+    const val ktx = "1.6.0"
+    const val kotlin = "1.4.31"
+
+    const val timber = "5.0.1"
+
+    const val firebaseAuthVersion = "17.0.0"
+    const val firebaseCoreVersion = "16.0.9"
+    const val firestoreKtxVersion = "19.0.0"
+
+
+    const val rxjava = "2.2.8"
+    const val rxAndroidVersion = "2.0.2"
+
+    const val gsonVersion = "2.8.0"
+    const val retrofit = "2.5.0"
+    const val loggingInterceptor = "3.12.1"
+    const val glide = "4.9.0"
+
+    const val lifecycle = "2.0.0"
+
+    const val playCore = "1.4.1"
+
+    const val junit = "4.12"
+    const val espresso = "3.0.2"
+    const val testRunner = "1.0.2"
+    const val assertjCore = "3.12.2"
+    const val mockitoKotlin = "2.1.0"
+    const val mockitoInline = "2.27.0"
+}
+
+object Libraries {
+    const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+    const val ktx = "androidx.core:core-ktx:${Versions.ktx}"
+
+    const val lifecycleRuntime = "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycle}"
+    const val lifecycleExtensions = "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycle}"
+    const val lifecycleCompiler = "androidx.lifecycle:lifecycle-compiler:${Versions.lifecycle}"
+    const val lifecycleKtx = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycle}"
+
+
+    const val supportFragmentKtx = "androidx.fragment:fragment-ktx:${Versions.supportFragment}"
+
+    const val navigationRuntimeKtx = "androidx.navigation:navigation-runtime-ktx:${Versions.navigation}"
+    const val navigationFragmentKtx = "androidx.navigation:navigation-fragment-ktx:${Versions.navigation}"
+    const val navigationUiKtx = "androidx.navigation:navigation-ui-ktx:${Versions.navigation}"
+    const val navigationSafeArgs = "androidx.navigation:navigation-safe-args-gradle-plugin:${Versions.navigation}"
+
+    const val preference = "androidx.preference:preference:${Versions.preferences}"
+
+    const val timber = "com.jakewharton.timber:timber:${Versions.timber}"
+
+    const val rxjava = "io.reactivex.rxjava2:rxjava:${Versions.rxjava}"
+    const val rxAndroid = "io.reactivex.rxjava2:rxandroid:${Versions.rxAndroidVersion}"
+
+
+    const val firebaseCore = "com.google.firebase:firebase-core:${Versions.firebaseCoreVersion}"
+    const val firebaseAuth = "com.google.firebase:firebase-auth:${Versions.firebaseAuthVersion}"
+
+    // This library transitively includes the firebase-firestore library
+    const val firestoreKtx = "com.google.firebase:firebase-firestore-ktx:${Versions.firestoreKtxVersion}"
+
+    const val gson = "com.google.code.gson:gson:${Versions.gsonVersion}"
+    const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+    const val rxjavaAdapter = "com.squareup.retrofit2:adapter-rxjava2:${Versions.retrofit}"
+    const val loggingInterceptor = "com.squareup.okhttp3:logging-interceptor:${Versions.loggingInterceptor}"
+    const val retrofitGsonConverter = "com.squareup.retrofit2:converter-gson:${Versions.retrofit}"
+
+    const val glide = "com.github.bumptech.glide:glide:${Versions.glide}"
+    const val glideCompiler = "com.github.bumptech.glide:compiler:${Versions.glide}"
+
+    const val dagger = "com.google.dagger:dagger:${Versions.dagger}"
+    const val daggerCompiler = "com.google.dagger:dagger-compiler:${Versions.dagger}"
+
+    const val daggerAndroid = "com.google.dagger:dagger-android:${Versions.dagger}"
+    const val daggerAndroidSupport = "com.google.dagger:dagger-android-support:${Versions.dagger}"
+    const val daggerAndroidProcessor = "com.google.dagger:dagger-android-processor:${Versions.dagger}"
+
+
+}
+
+object SupportLibraries {
+    const val appCompatX = "androidx.appcompat:appcompat:${Versions.appcompat}"
+    const val annotationX = "androidx.annotation:annotation:${Versions.annotation}"
+    const val material = "com.google.android.material:material:${Versions.material}"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+
+}
+
+object GoogleLibraries {
+    const val playCore = "com.google.android.play:core:${Versions.playCore}"
+
+
+}
+
+//object FirebaseLibraries {
+//   const val auth = "com.google.firebase:firebase-auth:${Versions.firebaseAuth}"
+//   const val core = "com.google.firebase:firebase-core:${Versions.firebaseCore}"
+//}
+
+object TestLibraries {
+    const val junit = "junit:junit:${Versions.junit}"
+    const val espressoCore = "com.android.support.test.espresso:espresso-core:${Versions.espresso}"
+    const val testRunner = "com.android.support.test:runner:${Versions.testRunner}"
+    const val assertjCore = "org.assertj:assertj-core:${Versions.assertjCore}"
+    const val mockitoKotlin = "com.nhaarman.mockitokotlin2:mockito-kotlin:${Versions.mockitoKotlin}"
+    const val mockitoInline = "org.mockito:mockito-inline:${Versions.mockitoInline}"
+    const val lifecycleTesting = "androidx.arch.core:core-testing:${Versions.lifecycle}"
+}
